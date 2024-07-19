fd6e2a68c403dc980474750074bb2a3ba6dc9656
jigoo <165612433+jisu0212@users.noreply.github.com>
2024-07-19 18:05:07 +0900

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,12 @@
+/* header-background */
+const head = document.querySelector('header')
+window.addEventListener('scroll',function(){
+    if(window.pageYOffset < 100){
+        head.style.backgroundColor = 'rgba(0,0,0,0)'
+        head.style.transition = 'background-color 0.3s'
+    }
+    if(window.pageYOffset >= 100){
+        head.style.backgroundColor = 'rgba(0,0,0,0.7)'
+        head.style.transition = 'background-color 0.3s'
+    }
+})
